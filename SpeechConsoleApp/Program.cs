db44f15123f510bc680f18f7e06e39e65de19f02
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2017-11-19 16:06:05 +0200

    Add SpeechConsoleApp

---
+++ SpeechConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SpeechConsoleApp
+{
+    using System.Speech.Synthesis;
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            var synth = new SpeechSynthesizer();
+            synth.Speak("This example speaks the text in a string.");
+        }
+    }
+}
